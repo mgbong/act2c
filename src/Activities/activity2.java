e5999cdb7e8e710601c1f4c56ad63c8353948032
AndreR2C <karuizawakei083@gmail.com>
2025-08-05 15:09:41 +0800

    bank

--- src/Activities/activity2.java
+++ src/Activities/activity2.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Activities;
 
-/**
- *
- * @author PC 22
- */
+import java.util.Scanner;
+
 public class activity2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    System.out.println("Hello Act 2");
+    }
 }
